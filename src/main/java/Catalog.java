1073d9e67d035f5ce982069de7cdd74408e5e2d2
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-04-09 15:23:35 +0900

    start : project init

---
+++ src/main/java/Catalog.java
@@ -0,0 +1,6 @@
+/**
+ * Created By iw.jhun
+ * On 2018-04-09 , 오후 3:21
+ */
+public class Catalog {
+}
---
+++ src/test/java/CatalogTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created By iw.jhun
+ * On 2018-04-09 , 오후 3:21
+ */
+public class CatalogTest {
+
+    private final Catalog catalog = new Catalog();
+
+    @Test
+    public void init(){
+
+    }
+
+}
